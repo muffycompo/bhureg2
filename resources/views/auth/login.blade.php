9f66849de0b2693732e89b21fea110dcd58710de
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2017-06-21 18:54:10 +0100

    Add BHU Logo to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            <div class="text-center">
+                <img src="{{ asset('images/bhu-logo.png') }}" style="height: 100px; margin-bottom: 20px;" alt="Bingham University Logo">
+            </div>
             <div class="panel panel-default">
                 <div class="panel-heading text-center">Students Course Registration Portal</div>
                 <div class="panel-body">
